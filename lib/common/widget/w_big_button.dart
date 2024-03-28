c46a6c5044f092c9037374556e72154a49fe265c
fixelChaeHyun <chra@fixelsoft.com>
2024-03-28 13:22:00 +0900

    feat: Pull to refresh 추가

--- lib/common/widget/w_big_button.dart
+++ lib/common/widget/w_big_button.dart
@@ -5,19 +5,22 @@ import 'package:flutter/material.dart';
 
 class BigButton extends StatelessWidget {
   final String text;
-  final VoidCallback  onTap;
+  final VoidCallback onTap;
 
   const BigButton(this.text, {super.key, required this.onTap});
 
   @override
   Widget build(BuildContext context) {
-    return RoundedContainer(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          text.text.white.size(20).bold.make(),
-          const Arrow(),
-        ],
+    return Tap(
+      onTap: onTap,
+      child: RoundedContainer(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            text.text.white.size(20).bold.make(),
+            const Arrow(),
+          ],
+        ),
       ),
     );
   }
--- lib/screen/main/tab/home/f_home.dart
+++ lib/screen/main/tab/home/f_home.dart
@@ -1,8 +1,10 @@
+import 'package:fast_app_base/common/cli_common.dart';
 import 'package:fast_app_base/common/common.dart';
 import 'package:fast_app_base/common/widget/round_button_theme.dart';
 import 'package:fast_app_base/common/widget/w_round_button.dart';
 import 'package:fast_app_base/common/widget/w_rounded_container.dart';
 import 'package:fast_app_base/screen/dialog/d_message.dart';
+import 'package:fast_app_base/screen/main/s_main.dart';
 import 'package:fast_app_base/screen/main/tab/home/vo/bank_accounts_dummy.dart';
 import 'package:fast_app_base/screen/main/tab/home/w_bank_account.dart';
 import 'package:fast_app_base/screen/main/tab/home/w_ttos_app_bar.dart';
@@ -23,27 +25,39 @@ class HomeFragment extends StatelessWidget {
       color: Colors.black,
       child: Stack(
         children: [
-          SingleChildScrollView(
-            padding: EdgeInsets.only(top: 60),
-            child: Column(
-              children: [
-                BigButton(
-                  "토스뱅크",
-                  onTap: () {
-                    context.showSnackbar("토스뱅크를 눌렀어요.");
-                  },
-                ),
-                height10,
-                RoundedContainer(child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    "자산".text.bold.white.make(),
-                    height5,
-                    ...bankAccounts.map((e) => BankAccountWidget(e) ).toList(),
-                  ],
-                )),
-              ],
-            ).pSymmetric(h: 20),
+          RefreshIndicator(
+            edgeOffset: TtossAppBar.appBarHeight,
+            onRefresh: () async {
+              print("Async start");
+              await sleepAsync(500.ms);
+              print("Async end");
+            },
+            child: SingleChildScrollView(
+              padding: const EdgeInsets.only(
+                top: TtossAppBar.appBarHeight,
+                bottom: MainScreenState.bottomNavigatorHeight,
+              ),
+              child: Column(
+                children: [
+                  BigButton(
+                    "토스뱅크",
+                    onTap: () {
+                      context.showSnackbar("토스뱅크를 눌렀어요.");
+                    },
+                  ),
+                  height10,
+                  RoundedContainer(
+                      child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      "자산".text.bold.white.make(),
+                      height5,
+                      ...bankAccounts.map((e) => BankAccountWidget(e)).toList(),
+                    ],
+                  )),
+                ],
+              ).pSymmetric(h: 20),
+            ),
           ),
           const TtossAppBar(),
         ],
